e078a123d9adc0e82a32cc3bd137d5a7b4187ea7
Marcio Zamperetti <marcioff6@hotmail.com>
2018-12-12 07:51:41 -0200

    Adicionado Blazor Components
    Adicionado Blazor Components para criação de gráficos

---
+++ BlazorComponents/ChartJS/ChartJSInterop.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorComponents.ChartJS
+{
+    public class ChartJSInterop
+    {
+        public static Task<bool> InitializeLineChart(ChartJSLineChart lineChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.InitializeLineChart", new[] { lineChart });
+        }
+
+        public static Task<bool> UpdateSize(string canvasId, int newWidth, int newHeight)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.UpdateSize", new object[] { canvasId, newWidth, newHeight });
+        }
+
+        public static Task<bool> InitializeBarChart(ChartJSBarChart barChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.InitializeBarChart", new[] { barChart });
+        }
+
+        public static Task<bool> UpdateLineChart(ChartJSLineChart lineChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.UpdateLineChart", new[] { lineChart });
+        }
+
+        public static Task<bool> UpdateBarChart(ChartJSBarChart barChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.UpdateBarChart", new[] { barChart });
+        }
+
+        public static Task<bool> InitializeRadarChart(ChartJSRadarChart radarChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.InitializeRadarChart", new[] { radarChart });
+        }
+
+        public static Task<bool> UpdateRadarChart(ChartJSRadarChart radarChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.UpdateRadarChart", new[] { radarChart });
+        }
+
+        public static Task<bool> InitializePieChart(ChartJSPieChart pieChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.InitializePieChart", new[] { pieChart });
+        }
+
+        public static Task<bool> UpdatePieChart(ChartJSPieChart pieChart)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("BlazorComponents.ChartJSInterop.UpdatePieChart", new[] { pieChart });
+        }
+
+    }
+}
